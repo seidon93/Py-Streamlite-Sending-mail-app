d5189cf81c5bed01bee49f13814bf6ef0d12e41d
Ondřej Seidl <seidon@seznam.cz>
2024-08-26 11:31:11 +0200

    add descriptions, images and links

--- main.py
+++ main.py
@@ -15,14 +15,20 @@ with col2:
 cont1 = st.container()
 cont1.write("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse luctus neque ut posuere lacinia. Vivamus semper non ante sit amet aliquet. Nam vitae nisi ante. Quisque nec ornare nisl. Vestibulum a felis a lacus vehicula vehicula.")
 
-
-col3, col4 = st.columns(2)
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pd.read_csv("data.csv", sep=";")
+
 with col3:
     for index, row in df[:10].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source]({row['url']})")
 
 with col4:
     for index, row in df[10:].iterrows():
         st.header(row["title"])
+        st.write(row["description"])
+        st.image("images/" + row["image"])
+        st.write(f"[Source]({row['url']})")

d4cddb60beb80c243b460c31bbfd6a7f6e43fb5d
Ondřej Seidl <seidon@seznam.cz>
2024-08-28 10:50:22 +0200

    assign the send email function to a button

--- Pages/Contact.py
+++ Pages/Contact.py
@@ -1,12 +1,19 @@
 import streamlit as st
-from streamlit import button
+from Send_mail import send_email
 
 st.header('Contact Page')
 
 with st.form(key='email_form'):
     user_email = st.text_input("Enter your email address")
     user_mess = st.text_area("Put in your message")
-    e_btn = st.form_submit_button('Submit')
-
+    message = f"""\n
+    Subject: New email from {user_email}
+    From: {user_email}
+    {user_mess}
+    """
+    button = st.form_submit_button("Submit")
     if button:
         print("Email Submitted")
+        send_email(message)
+        st.info('Email was sent!')
+
